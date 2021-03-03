775432bfa88a4863c71ca37b139116a4b04a0060
para29 <65754533+para29@users.noreply.github.com>
2021-03-03 18:00:52 +0000

    Comeco implementacao tabuleiro

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using Tabuleiro;
+using tabuleiro;
 
 namespace xadrez_console
 {
@@ -7,13 +7,11 @@ namespace xadrez_console
     {
         static void Main(string[] args)
         {
-            Posicao P;
+            Tabuleiro tab = new Tabuleiro(8, 8);
 
-            P = new Posicao(3, 4);
+           
 
-            Console.WriteLine("Posiçao: " + P);
-
-            Console.ReadLine();
+            Console.ReadLine(); 
 
 
         }
--- xadrez-console/Tabuleiro/Posicao.cs
+++ xadrez-console/Tabuleiro/Posicao.cs
@@ -1,6 +1,6 @@
 ﻿
 
-namespace Tabuleiro
+namespace tabuleiro
 {
     class Posicao
     {
