b4840bdec96ce347a0ee49e55cb81d34c66199e6
para29 <65754533+para29@users.noreply.github.com>
2021-03-10 18:19:35 +0000

    Ligar posiçoes

--- xadrez-console/Tabuleiro/Tabuleiro.cs
+++ xadrez-console/Tabuleiro/Tabuleiro.cs
@@ -20,6 +20,16 @@ namespace tabuleiro
             pecas = new Peca[linhas, colunas];
         }
 
+        public Peca peca(int linha, int coluna)
+        {
+            return pecas[linha, coluna];
+        }
+
+        public void colocarPeca(Peca p, Posicao pos)
+        {
+            pecas[pos.linha, pos.coluna] = p;
+            p.posicao = pos;
+        }
 
     }
 }
